e88f8640d3f2c1ab88cf8b4f969910d465245d88
Ben Halverson <benhalverson33@gmail.com>
2014-12-11 15:10:41 -0800

    Initial Commit

---
+++ UdacityApp1Tests/UdacityApp1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  UdacityApp1Tests.swift
+//  UdacityApp1Tests
+//
+//  Created by Ben Halverson on 12/11/14.
+//  Copyright (c) 2014 Ben Halverson. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class UdacityApp1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
