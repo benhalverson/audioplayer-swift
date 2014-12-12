9b5f1efeb66540e38fef8bbba9f03c65df5ccb87
Ben Halverson <benhalverson33@gmail.com>
2014-12-11 20:50:11 -0800

    Controller to play slow audio

---
+++ UdacityApp1/PlaySoundsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PlaySoundsViewController.swift
+//  UdacityApp1
+//
+//  Created by Ben Halverson on 12/11/14.
+//  Copyright (c) 2014 Ben Halverson. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func PlaySlowAudio(sender: UIButton) {
+        //TODO: playslow audio
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
