6a6b13602ab7f7507f5ba8dafb41c198b63245cc
Ben Halverson <benhalverson33@gmail.com>
2014-12-11 20:51:30 -0800

    added recording label stopbutton recordbutton

--- UdacityApp1/ViewController.swift
+++ UdacityApp1/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var StopButton: UIButton!
+    @IBOutlet weak var RecordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    override func viewWillAppear(animated: Bool) {
+        StopButton.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,11 +28,13 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(sender: UIButton) {
         //shows label on tap of button
+        RecordButton.enabled = false
         recordingLabel.hidden = false
+        StopButton.hidden = false
         //TODO: Record the user's voice
         println("in recordAudio")
     }
-    @IBOutlet weak var recordingLabel: UILabel!
+    
 
     @IBAction func stopRecording(sender: UIButton) {
         recordingLabel.hidden = true
